f9435ca9cfbfe72cb435e9e17c3595074da4c4c3
cogotti-giulia <63798366+cogotti-giulia@users.noreply.github.com>
2020-04-16 16:15:46 +0200

    Add files via upload

---
+++ scacchiera.php
@@ -0,0 +1,21 @@
+<html>
+    <body bgColor=yellow> 
+        <?php
+            echo "<br><br><br><br><br><br><br><br><br><br>"; //va a capo tre volte
+
+            for($r=0; $r<8; $r++){ //scorre le righe
+                /*align allinea la tabella al centro della pagina, rules=all toglie lo spazio tra le celle*/
+                echo "<table   align=center rules=all> <tr> ";
+                    for($c=0; $c<8; $c++){ //scorre le colonne
+                    
+                        if(($c+$r)%2==1)
+                            echo "<td height=50 width=50 bgColor=black>"; 
+                        else
+                            echo "<td height=50 width=50; bgColor=white>"; 
+                    }
+                echo "</tr> </table>";
+
+            }
+        ?>
+    </body>
+</html>
