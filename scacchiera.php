5f7fba2cf4c739c277d6065fa3b14283915ac981
cogotti-giulia <63798366+cogotti-giulia@users.noreply.github.com>
2020-05-21 09:59:07 +0200

    Update scacchiera.php

--- scacchiera.php
+++ scacchiera.php
@@ -1,11 +1,11 @@
 <?php
 echo "<html><body bgColor=yellow> ";
        
-            echo "<br><br><center><span style='color:#000080; 
+            echo "<br><br><center><span style='color: black; 
 		font-family: Comic Sans MS, cursive, sans-serif; 
-		font-size:350%; text-shadow: 4px 4px #ccccff ;'>HEROKU with GITHUB</span>"; 
+		font-size:350%; text-shadow: 2px 2px #lightgrey ;'>HEROKU with GITHUB</span>"; 
 			
-		
+		echo "<br><br><span style='font-family: monospace;'>made by <font color=#cc0000>Cogotti Giulia</font> - <font color=#000080>Matta Francesco<br><br>";
 
             for($r=0; $r<8; $r++){ //scorre le righe
                 /*align allinea la tabella al centro della pagina, rules=all toglie lo spazio tra le celle*/
